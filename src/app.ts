42a481927ef2fffff89dc82776d99511698484ae
akhil guttula <guttula.akhil@gmail.com>
2021-10-26 01:47:57 +0530

    added views support

--- src/app.ts
+++ src/app.ts
@@ -4,17 +4,16 @@ import path from "path";
 const app = express();  
 const port = 3000;
 
-app.all('/home', (req, res) => {
-  res.send('home page.')
-});
-
-console.log(__dirname)
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
 
-app.get('/users/:userId/books/:bookId', (req, res) => {
-  res.send(req.params);
-})
+app.get('/', (req, res) => {
+  res.render('index', {
+    "title": "Hello world"
+  })
+});
 
 app.listen(port, () => {
   console.log(`application running on port ${port}.`);
