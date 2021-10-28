e08991f7bbbc42be1ee6b8ac8add177812ae679c
akhil guttula <guttula.akhil@gmail.com>
2021-10-28 20:26:45 +0530

    heroku config changes

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 import path from "path";
 
 const app = express();
-const port = 5000;
+const port : string|number = process.env.PORT || 5000;
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
