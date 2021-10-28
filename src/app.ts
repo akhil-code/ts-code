572a48429030c96db27d7f26a78514d745cbb25c
akhil guttula <guttula.akhil@gmail.com>
2021-10-28 18:43:05 +0530

    Adding News feed UI and fixing npm scripts.

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,15 @@
-import express from 'express';
+import express from "express";
 import path from "path";
 
-const app = express();  
-const port = 3000;
+const app = express();
+const port = 5000;
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-app.use('/static', express.static(path.join(__dirname, 'public')));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
-
-app.get('/', (req, res) => {
-  res.render('index', {
-    "title": "Hello world"
-  })
+app.get("/", (req, res) => {
+  res.render("index", { title: "Instagram" });
 });
 
 app.listen(port, () => {
